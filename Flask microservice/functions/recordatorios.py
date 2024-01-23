000e093f83bfbba3863b88e4260cf2dfe379d8b6
Daniel Loredo <DanielLoredo>
2024-01-22 22:55:02 -0600

    Added IBM projectPaymentreminder model results

--- Flask microservice/functions/recordatorios.py
+++ Flask microservice/functions/recordatorios.py
@@ -1,57 +1,47 @@
-# import pandas as pd
-# from sklearn.preprocessing import StandardScaler
-# import tensorflow as tf
-# import numpy as np
+from datetime import datetime
+from helpers.users import get_user
+import pandas as pd
+import math
 
-# Load the model
-# model = tf.keras.models.load_model('calendario/content/calendario-model')
+meses = {
+    "January": "Enero",
+    "February": "Febrero",
+    "March": "Marzo",
+    "April": "Abril",
+    "May": "Mayo",
+    "June": "Junio",
+    "July": "Julio",
+    "August": "Agosto",
+    "September": "Septiembre",
+    "October": "Octubre",
+    "November": "Noviembre",
+    "December": "Diciembre"
+}
 
 def recordatorioAutomatico():
-    # Preparar datos para la prediccion de un modelo
-    # --------------------------------------
-    # data = {
-    #     'Fecha': ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04', '2023-01-05', '2023-01-06', '2023-01-07', '2023-01-08', '2023-01-09', '2023-01-10'],
-    #     'Cantidad': [100, 150, 200, 125, 175, 190, 210, 50, 300, 220],
-    #     'Concepto': ['Pago', 'Factura', 'Transferencia', 'Depósito', 'Compra', 'Pago', 'Factura', 'Transferencia', 'Depósito', 'Compra']
-    # }
-    # unique_concepts = ["Compra", "Pago", "Factura", "Transferencias", "Deposito"] # Definir diferencias a partir del archivo CSV
+    # Read file
+    file = "./data/projectPaymentReminder_MONTHLY.csv"
+    df = pd.read_csv(file)
 
-    # df = pd.DataFrame(data)
+    currentIdUser = "1234"
+    today = datetime.now().date()
 
-    # # Convertir la fecha
-    # df['Fecha'] = pd.to_datetime(df['Fecha'])
-    # df['Dias_desde_inicio'] = (df['Fecha'] - df['Fecha'].min()).dt.days
+    # Busca el usuario actual y los recordatorios correspondientes a hoy em "REMINDER_DATE"
+    filtro_usuario_dia = df[(df['CUST_NUM'] == int(currentIdUser)) & (df['REMINDER_DATE'] == today.strftime("%Y-%m-%d"))]
+    datosRecordatorio = filtro_usuario_dia.iloc[0]
 
-    # # Codificación one-hot para 'Concepto'
-    # conceptos_dummies = pd.get_dummies(df['Concepto'], prefix='Concepto')
-    # df = pd.concat([df, conceptos_dummies], axis=1)
+    # Cuenta destino:
+    acc_dest = get_user(str(datosRecordatorio["ACC_DEST"]))["Nombre"]
 
-    # # Normalizar la cantidad de cantidad
-    # scaler = StandardScaler()
-    # df['Cantidad_escalada'] = scaler.fit_transform(df[['Cantidad']])
+    # Monto (Prediccion):
+    pred_monto = math.ceil(datosRecordatorio["PRED_MONTO"])
 
-    # # Crear secuencia
-    # len_secuencia = 9
-    # caracteristicas = df[['Dias_desde_inicio', 'Cantidad_escalada']]
-    # secuencias, etiquetas = crear_secuencias(caracteristicas, len_secuencia)
-
-    # # La forma de 'secuencia_a_predecir' debe coincidir con la forma de entrada que el modelo espera
-    # secuencia_a_predecir = np.array(secuencias)  # Asegurarte de que tiene la forma adecuada, por ejemplo (1, 10, 2) si tu secuencia es de longitud 10 y tienes 2 características
-
-    # # data = request.json
-    # # prediction = model.predict(data['input'])
-
-    # prediccion = model.predict(secuencia_a_predecir)
-
-    # # Obtener la categoría predicha (el índice de la probabilidad más alta)
-    # categoria_predicha_idx = np.argmax(prediccion[0])
-    # categorias = list(unique_concepts)
-    # categoria_predicha = categorias[categoria_predicha_idx]
-
-    # print(f"El próximo pago se predice que será para el concepto: {categoria_predicha}")
+    # Dia actual en espanol
+    dia, mes = today.strftime("%d de %B").split(' de ')
+    fecha_recordatorio = f"{dia} de {meses.get(mes, mes)}"
 
     return {
-        "recordatorio": "Hola Jos, hoy es 26 de Febrero! ¿Quieres pagar $700.00 de 'Colegiatura Vale' como cada mes?"
+        "recordatorio": f"Hola Jos, hoy es {fecha_recordatorio}! ¿Quieres pagar ${pred_monto}.00 a la cuenta de '{acc_dest}' como cada mes?"
     }
 
 def recordatorioManual():
@@ -77,16 +67,3 @@ def tarjetasDisponibles():
             "Numero": "****1234"
         }
     ]
-
-# Secuenciar los datos
-def crear_secuencias(data, len_secuencia):
-    secuencias = []
-    etiquetas = []
-
-    for i in range(len(data) - len_secuencia):
-        secuencia = data.iloc[i:i+len_secuencia].values
-        etiqueta = data.iloc[i+len_secuencia].get('Concepto')
-        secuencias.append(secuencia)
-        etiquetas.append(etiqueta)
-
-    return secuencias, etiquetas
